0665daa1dc728e2a533b130a74064cf7cdc704f6
dlwortmann <dannywortmann@gmail.com>
2025-01-20 21:47:44 -0600

    defined HTML more clearly for specifications and started on form.js functions

--- Develop/assets/js/form.js
+++ Develop/assets/js/form.js
@@ -1,5 +1,32 @@
 // TODO: Create a variable that selects the form element
+let formInput = [
+    username: "",
+    title: "",
+    content: ""
+]
+// TODO: Create a function that handles the form submission.
+function formSubmission(event) {
+    event.preventDefault();
 
-// TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the `redirectPage` function. If the form is submitted with missing data, display an error message to the user.
+    const username = document.getElementById('username').value
+    const title = document.getElementById('title').value
+    const content = document.getElementById('content').value
+    
+    if (!username || !title || !content) {
+        alert("Please complete the form")
+        return;
+    }
+//Grab the form data and store it in local storage,
+    else {
+    formInput.username = username
+    formInput.title = title
+    formInput.content = content
+    localStorage.setItem('formInput', JSON.stringify(formInput))
+    }
+}
+
+//  then redirect to the blog page using the `redirectPage` function. If the form is submitted with missing data, display an error message to the user.
+
+
 
 // TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
--- Develop/assets/js/logic.js
+++ Develop/assets/js/logic.js
@@ -1,10 +1,23 @@
 // TODO: Create logic to toggle the light/dark mode styles for the page and circle. The mode should be saved to local storage.
+function darkMode() {
+   element.idList.toggle('darkMode')
 
+   
+}
 
 // TODO: Create a function called `readLocalStorage` that reads from local storage and returns the data. If no data exists, return an empty array.
 
+function readLocalStorage () {
+  
+
+}
+
 
 // TODO: Create a function called `storeLocalStorage` that takes a given object and saves the new data to the existing blog data in local storage.
+
+function storeLocalStorage () {
+  localStorage.setItem('inputArray', JSON.stringify(inputArray))
+}
 
 
 // ! Use the following function whenever you need to redirect to a different page
