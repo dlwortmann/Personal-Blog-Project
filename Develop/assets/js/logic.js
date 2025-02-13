6128f0692d1aec689573fd1fdc37bcb4ebc4dedc
dlwortmann <dannywortmann@gmail.com>
2025-02-13 13:37:21 -0600

    remove unnecessary comments

--- Develop/assets/js/logic.js
+++ Develop/assets/js/logic.js
@@ -41,37 +41,9 @@ function storeLocalStorage(blogEntry) {
   prevPosts.push(blogEntry)
   localStorage.setItem("blogEntry", JSON.stringify(prevPosts))
 } 
-//storeLocalStorage()
-
-
-
-//readLocalStorage funtion should return an array of objects(all the old posts), storeLocalstorage function should call read local storage function []=readLocalStorage, all old post stored as an array. Store local storage should be pushing new post to the end of the old array defined by readLocalStorage
 
 
 // TODO: Create a function called `storeLocalStorage` that takes a given object and saves the new data to the existing blog data in local storage.
-//function storeLocalStorage (formData) {
-  //let postArray = []
-  //readLocalStorage()
-  //postArray = JSON.parse(localStorage.getItem('blogData'))
-  //console.log(typeof(tempItem))
-  //if  (username === '' || 
-    //title === ''|| 
-    //content === '') {
-    //errorMessage.textContent = "Please complete the form."
-    // plug in variable.textContent = "Please complete the form."
-    //return false;
-  //}
-
-  // else {
-  //   //Grab the form data and store it in local storage,
-  //   console.log(formData)
-  //   console.log(postArray)
-  //   postArray.push(formData)
-  //   localStorage.setItem('blogData', JSON.stringify(postArray))
-  //   }
-
-//}
-
 
 // ! Use the following function whenever you need to redirect to a different page
 let redirectURL = '';
